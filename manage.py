220868a8f97323bf018fdd8ff7d2a6d24c4e11a6
igihozo-stella <igihozo96@gmail.com>
2020-12-06 19:15:57 +0200

    add the file to run the app.

---
+++ manage.py
@@ -0,0 +1,24 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Post,Comment
+from  flask_migrate import Migrate, MigrateCommand
+
+
+# Creating app instance
+app = create_app('development')
+# app = create_app('test')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests =unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=1).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
