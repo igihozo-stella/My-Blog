a4b82a07df4724111edeaea3dbced8c90b5556bb
igihozo-stella <igihozo96@gmail.com>
2020-12-07 10:24:28 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     QUOTES_URL='http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY="12345"
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://stella:marlyne96@localhost/myblog'
     SQLALCHEMY_TRECK_MODIFICATIONS=True
     UPLOADED_PHOTOS_DEST ='app/static/photos'
