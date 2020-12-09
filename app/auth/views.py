9df6959fabe2a2df94b7b6679d5d6e048987cbdc
igihozo-stella <igihozo96@gmail.com>
2020-12-09 11:11:36 +0200

    last commit.

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,6 +31,6 @@ def register():
     if form.validate_on_submit():
         user=User(email=form.email.data, username=form.username.data,password=form.password.data)
         user.save_u()
-        mail_message("Welcome to this Blog","email/welcome_user",user.email,user=user)
+        # mail_message("Welcome to this Blog","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html',registration_form=form)
